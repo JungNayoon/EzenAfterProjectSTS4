918b39cffdb1031eaed09f8da839305b85dc507e
Auden Kim <sasameyuki03@naver.com>
2023-04-29 15:24:48 +0900

    2023-04-29 (재국) 프로젝트 세팅중

---
+++ src/test/java/ezen/project/afterproject/AfterprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package ezen.project.afterproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfterprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
