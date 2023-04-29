d0357c1dbd3cb83fb20e92f5c32ea1f86fab107c
Auden Kim <sasameyuki03@naver.com>
2023-04-29 15:35:00 +0900

    2023-04-29 (재국) 프로젝트 세팅 테스트 (1)

--- src/main/java/ezen/project/afterproject/AfterprojectApplication.java
+++ src/main/java/ezen/project/afterproject/AfterprojectApplication.java
@@ -8,6 +8,7 @@ public class AfterprojectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AfterprojectApplication.class, args);
+		System.out.println("123");
 	}
 
 }
